d6939cfdd7a9737b88bd964ce93f66603591e274
jmsche <contact@jmsche.fr>
2023-02-20 11:29:28 +0100

    Fix tests

--- tests/Form/SlugTypeTest.php
+++ tests/Form/SlugTypeTest.php
@@ -45,14 +45,14 @@ final class SlugTypeTest extends KernelTestCase
         $rendered = $this->twig->render('form.html.twig', ['form' => $form->createView()]);
 
         self::assertSame(
-            '<form name="form" method="post"><div id="form"><div><label for="form_name" class="required">Name</label><input type="text" id="form_name" name="form[name]" required="required" /></div><div><label for="form_slug" class="required">Slug</label><div
+            '<form name="form" method="post"><div id="form"><div><label for="form_name" class="required">Name</label><input type="text" id="form_name" name="form[name]" required="required" /></div><div><label for="form_slug" class="required">Slug</label>    <div
         class="input-group"
-        data-controller="leapt-slug"
-        data-leapt-slug-target-value="form_name"
-        data-leapt-slug-alert-message-value="If you change the slug, you can break links on other pages."
+        data-controller="leapt--slug-type-bundle--slug"
+        data-leapt--slug-type-bundle--slug-target-value="form_name"
+        data-leapt--slug-type-bundle--slug-alert-message-value="If you change the slug, you can break links on other pages."
    >
         <input type="text" id="form_slug" name="form[slug]" required="required" />
-        <button class="btn btn-outline-secondary" type="button" data-leapt-slug-target="button">
+        <button class="btn btn-outline-secondary" type="button" data-leapt--slug-type-bundle--slug-target="button">
             &#128274;
         </button>
     </div></div></div></form>
